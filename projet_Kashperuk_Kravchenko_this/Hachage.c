24f395a8881d10defc33be4288abe753671a678e
KristinaUkr <146866980+KristinaUkr@users.noreply.github.com>
2024-05-03 16:33:22 +0000

    ffff

--- projet_Kashperuk_Kravchenko_this/Hachage.c
+++ projet_Kashperuk_Kravchenko_this/Hachage.c
@@ -159,3 +159,15 @@ void libererTable(TableHachage *tab) {
         }
     }
 }
+/*
+int main(){
+    
+    for(int x = 1; x<11; x++){
+        for(int y = 1; y<11; y++){
+            printf("%d ",fonctionClef(x,y));
+        }
+    }
+
+    return 0;
+}
+*/
